87beeb4ac557d36dac7980c4f648d35106bc575f
David <dhahn123@gmail.com>
2019-04-08 21:57:52 -0700

    add image into App.js, myStyles.css

--- app/src/index.js
+++ app/src/index.js
@@ -5,7 +5,8 @@ import './myStyles.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
+      <img alt="header" src="/dist/images/header.jpg" className="app-header" />
       <p>
         We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
         have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
